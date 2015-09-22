f3d06f21c6b0b7764decd994aa68e82295fe3038
ksetiya <k.setiya@student.tudelft.nl>
2015-09-22 06:20:34 -0400

    added public to username input

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,7 +52,7 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">Name</label>
 							<div class="col-md-6">
-								<input type="text" class="form-control" placeholder="Choose a username" name="name" value="{{ old('name') }}">
+								<input type="text" class="form-control" placeholder="Choose a public username" name="name" value="{{ old('name') }}">
 							</div>
 						</div>
 
